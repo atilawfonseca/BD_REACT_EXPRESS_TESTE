50abeb4cd99a5d37507343e6b6e33d2549b80b1b
Atila W Fonseca <87386434+atilawfonseca@users.noreply.github.com>
2023-03-17 16:26:46 -0300

    progredindo com bd

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,57 @@
-import './App.css';
+import "./App.css";
+import Axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [moviename, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+  const [movieList, setMovieList] = useState('');
+
+  useEffect(() => {
+    Axios.get("http://localhost:5000/api/get").then((response) => {
+      setMovieList(response.data);
+    });
+  });
+
+  const submitReview = async (e) => {
+    Axios.post("http://localhost:5000/api/insert", {
+      movieName: moviename,
+      movieReview: review,
+    }).then(() => {
+      alert("Sucessful insert");
+    });
+  };
+
   return (
     <div className="App">
-
       <h1>CRUD APPLICATION</h1>
-      <form className='form'>
+      <form className="form">
         <label>Movie Name:</label>
-        <input type="text" name="movieName"></input>
+        <input
+          type="text"
+          name="movieName"
+          onChange={(e) => {
+            setMovieName(e.target.value);
+          }}
+        />
         <label>Review: </label>
-        <input type="text" name="review" ></input>
+        <input
+          type="text"
+          name="review"
+          onChange={(e) => {
+            setReview(e.target.value);
+          }}
+        ></input>
 
-        <button>Submit</button>
-
+        <button onClick={submitReview}>Submit</button>
       </form>
-      
+
+      {/* {movieList.map((val) => {
+        return (<h1> MovieName: {val.movieName} || MovieReview : {val.movieReview}</h1>);
+      })} */}
+
     </div>
+    
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,27 +1,63 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require('cors'); 
 const app = express();
-const port = 5000; 
+const port = 5000;
 
-const db = mysql.createPool({
-    host: 'localhost',
-    port:'3306',
-    user: 'root',
-    password: 'At1l@040789',
-    database:'cruddatabase',
-
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "At1l@040789",
+  database: "cruddatabase",
+  port: "3306",
 });
 
+db.connect();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', (req, res) => {
-    const sqlInsert = "INSERT INTO `cruddatabase`.`movie_reviews` (`movieName`, `movieReview`) VALUES ('aprendendo mysql', 'na luta');"
-    db.query(sqlInsert, (erro, result) => {
-        res.send("Hello Word! parece que funciona, só que não");
-    } )
+//aqui tu envia uma req e recebe uma resp. Nesta requisição já envia uma query ao BD. Na resposta do bd imprime na tela da pagina o resultado do bd. 
+app.get("/api/get", (req, resp) => {
 
+  const sqlSelect = "SELECT * FROM cruddatabase.movie_reviews";
+  db.query(sqlSelect, (error, result) => {
+    resp.send(result);
+  })
+})
+
+
+app.post("/api/insert", (req, resp) => {
+
+  const movieName = req.body.movieName; 
+  const movieReview = req.body.movieReview;
+
+  const sqlInsert = "INSERT INTO `cruddatabase`.`movie_reviews` (`movieName`, `movieReview`) VALUES (?, ?)";
+  db.query(sqlInsert, [movieName, movieReview], (error, result) => {
+    console.log(result);
+  })
+  
+  
+});
+
+/* O NODE trabalha de forma assincrona, apesar do conection.end() estava no final, houve um erro na comunicação.*/
+app.get("/", (req, res) => {
+  const sqlInsert =
+    "INSERT INTO `cruddatabase`.`movie_reviews` (`movieName`, `movieReview`) VALUES ('não fui deletado', 'primeiro passo já foi');";
+  db.query(sqlInsert, (erro, result) => {
+    if (erro) throw erro;
+    res.send(`"tudo certo agora!`);
     
+  });
 });
+
+
 
 app.listen(port, () => {
-    console.log("running on port 5000");
+  console.log("running on port 5000");
 });
+
+
+
+
